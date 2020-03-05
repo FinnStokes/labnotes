42506acc17bb4196f43e417495c99318cebc95c7
Finn Stokes <finn.stokes@gmail.com>
2020-03-05 23:24:58 +0100

    Added math support using katex. Closes #1
    Had to remove ammonia sanitization.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(proc_macro_hygiene)]
+#![feature(proc_macro_hygiene, str_strip)]
 
 use std::borrow::Cow;
 use std::convert::TryFrom;
@@ -83,6 +83,9 @@ impl Note<'_> {
     pub fn render_html(&self) -> Markup {
         html! {
             (DOCTYPE)
+            head {
+                link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css" integrity="sha384-zB1R0rpPzHqg7Kpt0Aljp8JPLqbXI3bhnPWROx27a9N0Ll6ZP/+DiW/UqRcLbRjq" crossorigin="anonymous";
+            }
             body {
                 (self.body)
             }
--- src/markdown.rs
+++ src/markdown.rs
@@ -1,5 +1,5 @@
 use maud::{Markup, PreEscaped, Render};
-use pulldown_cmark::{html, Options, Parser};
+use pulldown_cmark::{html, CodeBlockKind, CowStr, Event, Options, Parser, Tag};
 
 /// Renders a block of Markdown using `pulldown-cmark`.
 pub struct Markdown<T: AsRef<str>>(pub T);
@@ -7,11 +7,66 @@ pub struct Markdown<T: AsRef<str>>(pub T);
 impl<T: AsRef<str>> Render for Markdown<T> {
     fn render(&self) -> Markup {
         // Generate raw HTML
-        let mut unsafe_html = String::new();
         let parser = Parser::new_ext(self.0.as_ref(), Options::all());
+
+        let mut katex = KatexMiddleware::new();
+        let parser = parser.filter_map(move |e| katex.map(e));
+
+        let mut unsafe_html = String::new();
         html::push_html(&mut unsafe_html, parser);
+
         // Sanitize it with ammonia
-        let safe_html = ammonia::clean(&unsafe_html);
-        PreEscaped(safe_html)
+        //let safe_html = ammonia::clean(&unsafe_html);
+        PreEscaped(unsafe_html)
+    }
+}
+
+struct KatexMiddleware {
+    tags: usize,
+}
+
+impl KatexMiddleware {
+    fn new() -> KatexMiddleware {
+        KatexMiddleware { tags: 0 }
+    }
+
+    fn map<'a>(&'_ mut self, event: Event<'a>) -> Option<Event<'a>> {
+        match event {
+            Event::Start(Tag::CodeBlock(CodeBlockKind::Fenced(kind))) => {
+                if kind.as_ref() == "math" {
+                    self.tags += 1;
+                    None
+                } else {
+                    Some(Event::Start(Tag::CodeBlock(CodeBlockKind::Fenced(kind))))
+                }
+            }
+            Event::End(Tag::CodeBlock(CodeBlockKind::Fenced(kind))) => {
+                if kind.as_ref() == "math" {
+                    self.tags -= 1;
+                    None
+                } else {
+                    Some(Event::End(Tag::CodeBlock(CodeBlockKind::Fenced(kind))))
+                }
+            }
+            Event::Text(text) => {
+                if self.tags > 0 {
+                    let opts = katex::Opts::builder().display_mode(true).build().unwrap();
+                    Some(Event::Html(CowStr::from(
+                        katex::render_with_opts(text.as_ref(), opts).unwrap(),
+                    )))
+                } else {
+                    Some(Event::Text(text))
+                }
+            }
+            Event::Code(code) => {
+                let s = code.as_ref();
+                if let Some(text) = s.strip_prefix("$").and_then(|s| s.strip_suffix("$")) {
+                    Some(Event::Html(CowStr::from(katex::render(text).unwrap())))
+                } else {
+                    Some(Event::Code(code))
+                }
+            }
+            e => Some(e),
+        }
     }
 }
